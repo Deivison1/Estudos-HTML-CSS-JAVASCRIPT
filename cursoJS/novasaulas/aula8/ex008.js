cfd717a36afe26897727139eee88b1acd482d1d3
Deivison1 <santosdeivison480@gmail.com>
2022-04-02 15:10:50 -0300

    Cindições compostas

--- cursoJS/novasaulas/aula8/ex008.js
+++ cursoJS/novasaulas/aula8/ex008.js
@@ -1,1 +1,6 @@
-console.log(' O console funcionou corretamente!')
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){ //condição simples!
+    console.log(` voce ultrapasou a velocidade máxima: Multado!!`)
+}
+console.log('Dirija sempre com sinto de segurança')
---
+++ cursoJS/novasaulas/aula8/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`vivendo no ${país}`)
+if( país == 'Brasil'){
+    console.log(' Você é Basileiro!')
+}else{
+    console.log(' Você é Extrangeiro')
+}
