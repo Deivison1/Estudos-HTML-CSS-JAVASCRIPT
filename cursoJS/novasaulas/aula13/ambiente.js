18ae189b2c10935b31b517c8e8ee39a949684b22
Deivison1 <santosdeivison480@gmail.com>
2022-04-09 18:26:25 -0300

    atualizando

--- cursoJS/novasaulas/aula13/ambiente.js
+++ cursoJS/novasaulas/aula13/ambiente.js
@@ -1,12 +1,21 @@
-var c = 1
-while (c <= 6){
-    console.log(`Passo ${c}`)
-    c++ //c = c + 1
+/*Repetições em Javascript
+
+Estrutura de repetição com while:
+teste lógico no inicio*/
+
+var c = 1 
+while( c <= 6) // Teste lógico no inicio
+{
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
 }
 
+/*Estrutura de repetição com (do while):
+  primeiro ele executa o bloco depois 
+  verifica se a condição é verdadeira */
 
-
-
-
-
-
+var c = 1 
+do{ 
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 6) // Teste lógico no final
