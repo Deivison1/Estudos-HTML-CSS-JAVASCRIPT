7813af814972909dd0e0917d510df1d57b92ff9e
Deivison1 <santosdeivison480@gmail.com>
2022-06-06 17:00:37 -0300

    Atualizando imagens

--- html5/main.js
+++ html5/main.js
@@ -2,33 +2,49 @@ const intems = [
     {
         id:0,
         nome: 'Luvas',
-        img: 'Luvas.png',
+        img: 'img\Luvas.png',
         quantidade: 0
     },
     {
         id:1,
         nome: 'Shorts',
-        img: 'Shorts.png',
+        img: 'img\Shorts.png',
         quantidade: 0
     },
     {
         id: 2,
         nome: 'Sapato',
-        img: 'Sapato.png',
+        img: 'img\Sapato.png',
         quantidade: 0
     },
 ]
 incinializarLoja = () => {
-    var containerProdutos = document.getElementById('pordutos');
+    var containerProdutos = document.getElementById('produtos');
     intems.map((val)=>{
-        containerProdutos.innerHTML+= `
+        containerProdutos.innerHTML += `
         <div class="produto-single">
-            <img src="`+val.img+`"<img/>
             <p>`+val.nome+`</p>
-            <a key="`+val.id+`" href="">Adicioanr ao carinho!</a>
+            <img src="`+val.img+`"<img/>
+            <a key="`+val.id+`" href="#">Adicioanr ao carinho!</a>
         </div>
 
         `;
     })
 }
 incinializarLoja();
+
+atualizarCarinho = () => {
+    console.log(intems);
+}
+
+var links = document.getElementsByTagName('a');
+
+for( var i = 0;  i < links.length; i++){
+    links[i].addEventListener('click', function(){
+        let key = this.getAttribute('key');
+        intems[key].quantidade++;
+        atualizarCarinho();
+        return false;
+    })
+
+}
