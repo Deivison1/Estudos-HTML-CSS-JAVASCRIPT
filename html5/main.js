5498df7e568cfb5ed476fc751ffdec890e3eae85
Deivison1 <santosdeivison480@gmail.com>
2022-06-09 17:15:35 -0300

    Estilizando carrinho

--- html5/main.js
+++ html5/main.js
@@ -3,18 +3,21 @@ const items = [
         id:0,
         nome: 'Camiseta modelo 1',
         img: 'Camiseta.png',
+        preco: 35.50,
         quantidade: 0
     },
     {
         id:1,
         nome: 'Camiseta modelo 2',
         img: 'Camiseta.png',
+        preco: 40.50,
         quantidade: 0
     },
     {
         id: 2,
         nome: 'Camiseta modelo 3',
         img: 'Camiseta.png',
+        preco: 42.50,
         quantidade: 0
     },
 ]
@@ -25,6 +28,7 @@ incinializarLoja = () => {
         <div class="produto-single">
             <img src="`+val.img+`"<img/>
             <p>`+val.nome+`</p>
+            <p>Valor: R$ `+val.preco+`</p>
             <a class= "link-carrinho"key="`+val.id+`" href="#carrinho">Adicionar ao carrinho!</a>
         </div>
 
@@ -42,21 +46,22 @@ atualizarCarrinho = () => {
         if(val.quantidade > 0){
         containerCarrinho.innerHTML += `
         <div class="info-carrinho">
-            <p style="float:left;">Produto: `+val.nome+`</p>
-            <p style="float: right;">Quantidade: `+val.quantidade+`</p>
-            <div style="clear:both"></div>
+            <p>Produto: `+val.nome+`</p>
+            <p>Qtde: `+val.quantidade+`</p>
+            <p>Preço:`+val.preco+`</p>
         </div>
         `
         };
     })
 }
 var links = document.getElementsByTagName('a');
+
     for(var i = 0; i < links.length; i++){
         links[i].addEventListener('click', function(){
             let key = this.getAttribute('key');
             items[key].quantidade++;
             atualizarCarrinho();
             return false;
-        })  
+        }) 
     }
 
